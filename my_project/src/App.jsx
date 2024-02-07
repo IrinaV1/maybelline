aa7af19825ffd0dda700947e329a30f445e2d7f7
Irina <vyustirina@gmail.com>
2024-02-08 00:03:56 +0300

    added header component

---
+++ my_project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './App.css'
+import Header from './components/Header/Header'
+
+function App () {
+  return (
+    <div className='container'>
+    <Header />
+    <main>kokih</main>
+    <footer>jkbkjb</footer>
+      </div>
+  )}
+
+export default App
---
+++ my_project/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../Header/index.css'
+function Header() {
+  return (
+    <div className='header '>
+        <div className="header_banner"></div>
+        <div className="header_wrapper">
+            <div className="header_content">
+                <div className="header_logo"> <a href="/"><img className='logo_img' src="https://www.maybelline.com/-/media/project/loreal/brand-sites/mny/master/demo/logos-logo-small.svg?rev=-1" width="155" height="24" alt="Maybelline New York | Go to Home Page" /></a></div>
+                <div className="header_nav"></div>
+            </div>
+            </div>
+        
+      ugkbgliu
+    </div>
+  )
+}
+
+export default Header
