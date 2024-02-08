00ded727830260e9696e2b153f189c4ca9b6f5da
Irina <vyustirina@gmail.com>
2024-02-09 00:04:59 +0300

    added header list

--- my_project/src/components/Header/Header.jsx
+++ my_project/src/components/Header/Header.jsx
@@ -7,7 +7,21 @@ function Header() {
         <div className="header_wrapper">
             <div className="header_content">
                 <div className="header_logo"> <a href="/"><img className='logo_img' src="https://www.maybelline.com/-/media/project/loreal/brand-sites/mny/master/demo/logos-logo-small.svg?rev=-1" width="155" height="24" alt="Maybelline New York | Go to Home Page" /></a></div>
-                <div className="header_nav"></div>
+                <div className="header_nav">
+                  <nav>
+                    <ul className='nav_li'>
+                      <li className='nav_l1'>VIRTUAL BEAUTY STUDIO</li>
+                      <li className='nav_l1'>NEW</li>
+                      <li className='nav_l1'>COLLECTIONS</li>
+                      <li className='nav_l1'>EYE MAKEUP</li>
+                      <li className='nav_l1'>FACE MAKEUP</li>
+                      <li className='nav_l1'>LIP MAKEUP</li>
+                      <li className='nav_l1'>MAKEUP TIPS</li>
+                      <li className='nav_l1'>BRAVE TOGETHER</li>
+                      <li className='nav_l1 double'>VALENTINE'S DAY</li>
+                    </ul>
+                  </nav>
+                </div>
             </div>
             </div>
         
